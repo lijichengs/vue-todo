54daaae92641066c07f3e88104863decdd87721b
lijichengs <lijicheng.me@outlook.com>
2019-01-30 14:12:27 +0800

    环境搭建

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="test">{{test}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'Vue Learning'
+        }
+    }
+}
+</script>
+<style>
+    #test {
+        color: red;
+    }
+</style>
