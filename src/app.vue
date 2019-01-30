423fd3c1b6f8acc40ca218f67fde5423495ace39
lijichengs <lijicheng.me@outlook.com>
2019-01-30 17:58:57 +0800

    Header

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,32 @@
 <template>
-    <div id="test">{{test}}</div>
+    <div id="app">
+        <div id="cover"></div>
+        <Header></Header>
+    </div>
 </template>
 
 <script>
+import Header from './todo/header.vue'
 export default {
-    data() {
-        return {
-            text: 'Vue Learning'
-        }
-    }
+   components: {
+       Header
+   }
 }
 </script>
-<style>
-    #test {
-        color: red;
-    }
+<style lang="stylus" scoped>
+#app
+    position absolute 
+    left 0
+    right 0
+    top 0
+    bottom 0
+#cover
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #999 
+    opacity .9
+    z-index -1      
 </style>
